90a05d743e74f6bb6ff7ef9a0be0426c7e647218
shree <shree@shree-PC>
2022-06-15 16:03:15 +0530

    by abc

--- GitHub/src/githubDemo/Demo1.java
+++ GitHub/src/githubDemo/Demo1.java
@@ -1,5 +1,10 @@
 package githubDemo;
 
 public class Demo1 {
-
+public static void main(String[] args) {
+	System.out.println("Git Demo");
+}
+public void m1() {
+	System.out.println("hello git");
+}
 }
